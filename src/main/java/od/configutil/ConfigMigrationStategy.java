5d5b30ba06feaf084f9a84726e3107ce149d0f79
nick <nick@899a1914-839d-7b46-afe5-da559765d5e5>
2010-11-18 13:48:55 +0000

    pom for configutil
    git-svn-id: https://www.objectdefinitions.com/od-repos/odrepository/trunk@1229 899a1914-839d-7b46-afe5-da559765d5e5

---
+++ src/main/java/od/configutil/ConfigMigrationStategy.java
@@ -0,0 +1,13 @@
+package od.configutil;
+
+/**
+ * Created by IntelliJ IDEA.
+* User: Nick Ebbutt
+* Date: 29-Apr-2010
+* Time: 14:38:26
+*/
+public interface ConfigMigrationStategy {
+
+    public String migrate(String configKey, String source);
+
+}
